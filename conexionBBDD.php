a62d7d42e2f76c14c168e69429c1ce12c0972503
M0ANGEL <riascomiguelangel53@gmail.com>
2024-04-28 13:38:22 -0500

    camio de zamo

---
+++ conexionBBDD.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conexion</title>
+</head>
+
+<body>
+
+    <!-- echo 'Current PHP version: ' . phpversion(); vewr vesrion de php -->
+
+    <?php
+
+    //pasos para conectar con base de datos
+    //forma con procedieminto
+/* 
+    $db_host = "localhost";      //1  
+    $db_usuario = "root";        //2 
+    $db_contra = "";             //3
+    $db_nombre = "empresa";     //4 
+
+    $conexion=mysqli_connect($db_host,$db_usuario,$db_contra,$db_nombre); */
+
+    phpinfo()
+
+  
+
+
+
+
+
+
+
+
+    ?>
+
+</body>
+
+</html>
